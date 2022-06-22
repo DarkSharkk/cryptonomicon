da5f0b214b67f15199e50c82093bac710bd38d53
Гамидов Шамсудин Селимханович <shsgamidov@edu.hse.ru>
2022-06-22 17:23:13 +0300

    Add saving to LocalStorage

--- src/App.vue
+++ src/App.vue
@@ -140,25 +140,38 @@ export default {
       graph: []
     };
   },
+  created() {
+    const tickersData = localStorage.getItem("cryptonomicon-list");
+    if (tickersData) {
+      this.tickers = JSON.parse(tickersData);
+      this.tickers.forEach((ticker) => {
+        this.subscribeToUpdates(ticker.name);
+      });
+    }
+  },
   methods: {
+    subscribeToUpdates(tickerName) {
+      setInterval(async () => {
+        const f = await fetch(
+          `https://min-api.cryptocompare.com/data/price?fsym=${tickerName}&tsyms=USD&api_key=cf1ffe5066b42b3d620becc1d25614809e9cceb16f9e8879d79ad0eb6aa336f6`
+        );
+        const data = await f.json();
+        // currentTicker.price = data.USD > 1 ? data.USD.toFixed(2) : data.USD.toPrecision(2);
+        this.tickers.find((t) => t.name === tickerName).price =
+          data.USD > 1 ? data.USD.toFixed(2) : data.USD.toPrecision(2);
+        if (this.sel?.name === tickerName) {
+          this.graph.push(data.USD);
+        }
+      }, 4000);
+    },
     add() {
       let currentTicker = {
         name: this.ticker,
         price: "-"
       };
       this.tickers.push(currentTicker);
-      setInterval(async () => {
-        const f = await fetch(
-          `https://min-api.cryptocompare.com/data/price?fsym=${currentTicker.name}&tsyms=USD&api_key=cf1ffe5066b42b3d620becc1d25614809e9cceb16f9e8879d79ad0eb6aa336f6`
-        );
-        const data = await f.json();
-        // currentTicker.price = data.USD > 1 ? data.USD.toFixed(2) : data.USD.toPrecision(2);
-        this.tickers.find((t) => t.name === currentTicker.name).price =
-          data.USD > 1 ? data.USD.toFixed(2) : data.USD.toPrecision(2);
-        if (this.sel?.name === currentTicker.name) {
-          this.graph.push(data.USD);
-        }
-      }, 4000);
+      localStorage.setItem("cryptonomicon-list", JSON.stringify(this.tickers));
+      this.subscribeToUpdates(currentTicker.name);
       this.ticker = "";
     },
     select(ticker) {
